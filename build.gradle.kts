6de822df818244ec8695bc2476f9609f2a4b7d58
marcelkoopman <marcel.koopman@gmail.com>
2024-02-13 19:19:33 +0100

    logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ repositories {
 
 dependencies {
     implementation("org.apache.kafka:kafka-clients:3.6.1")
+    implementation("org.slf4j:slf4j-api:2.0.12")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
 }
 
 application {
--- src/main/java/sidecar/Main.java
+++ src/main/java/sidecar/Main.java
@@ -1,8 +1,13 @@
 package sidecar;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 public class Main {
+   private static final Logger logger 
+      = LoggerFactory.getLogger(Main.class);
 
    public static void main(String[] args) {
-
+      logger.info("Sidecar started");
    }
 }
