d7678b6767393859956b53567c1d61429492981b
marcelkoopman <marcel.koopman@gmail.com>
2024-12-03 20:50:25 +0100

    Refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
+val kafkaClientVersion = "3.9.0"
+val logBackVersion = "1.5.12"
+val sl4jVersion = "2.1.0-alpha1"
+val avroVersion = "1.12.0"
+
 plugins {
     application 
     id ("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
+    id ("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id ("com.github.ben-manes.versions") version "0.51.0"
 }
 
 repositories {
@@ -8,10 +15,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.kafka:kafka-clients:3.6.1")
-    implementation("org.slf4j:slf4j-api:2.0.12")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
-    implementation("org.apache.avro:avro:1.11.3")
+    implementation("org.apache.kafka:kafka-clients:$kafkaClientVersion")
+    implementation("org.slf4j:slf4j-api:$sl4jVersion")
+    implementation("ch.qos.logback:logback-classic:$logBackVersion")
+    implementation("org.apache.avro:avro:$avroVersion")
 
 }
 
@@ -24,6 +31,6 @@ tasks.named<Test>("test") {
 }
 
 java {                                      
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
